cf214a9476737bc6a4020aa60974d0a859e03b90
GammePrati <gammegolden@gmail.com>
2020-11-23 08:43:17 -0800

    Create aClock.js

---
+++ aClock.js
@@ -0,0 +1,19 @@
+function setup(){
+    createCanvas(windowwidth, windowHeight);
+}
+function draw(){
+
+}
+
+function time()
+{
+    textAlign(CENTER, CENTER);
+    let h = hour();
+    let m = minute();
+    let s = second();
+    let n = h >= 12? " PM" : " AM";
+    if(m < 10)
+        m = "0"+m;
+    h%=12
+    text(h+":"+m+":"+s+n, width/2, height/2);
+}
